f04ebd75deefa3a4972af3256fdfe164c5345c55
Harinath V <140420368+Harivijay93@users.noreply.github.com>
2025-08-18 12:29:53 +0530

    Add files via upload

--- Nodejs/Module/modules.js
+++ Nodejs/Module/modules.js
@@ -1,4 +1,13 @@
 const imports =require('./data'); //require runs the file given path
 
-console.log(imports);
 console.log(imports.studentname);
+
+//Another method
+const {studentname,ages} =require('./data');
+
+console.log(studentname,ages);
+
+
+// Default modules
+const os =require('os');
+console.log(os);  //homedir()
