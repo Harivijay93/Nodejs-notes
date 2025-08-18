c8bd40f4d1b30f947ae22b4cefeeb31495bc5722
Harinath V <140420368+Harivijay93@users.noreply.github.com>
2025-08-18 10:59:41 +0530

    Add files via upload

--- Nodejs/hello.js
+++ Nodejs/hello.js
@@ -1,11 +1,30 @@
-// const hello = (hname)=>{
-//     console.log(`Hello, ${hname} !`);
-// }
-// hello("Hari")
+const hello = (hname)=>{
+    console.log(`Hello, ${hname} !`);
+}
+hello("Hari")
 
 // Global Object
-// console.log(global);
+console.log(global);
 
 setTimeout(()=>{
     console.log("Hello world");    //this is a timeout function it will run this string after 2 sec
 },2000)
+
+
+setInterval(()=>{
+    console.log('Interval Functtion'); //It will this code every 1 sec
+},1000)
+
+// clearInterval function
+setTimeout(()=>{
+    console.log("Hello world"); 
+    clearInterval(intfunc)   
+},5000)
+
+
+const intfunc=setInterval(()=>{
+    console.log('Interval Functtion');
+},1000)
+
+console.log(__dirname);
+console.log(__filename);
