2c0c28920b263b9646564538b5eb079eab3cab6f
Harinath V <140420368+Harivijay93@users.noreply.github.com>
2025-08-18 10:33:20 +0530

    Add files via upload

---
+++ Nodejs/hello.js
@@ -0,0 +1,11 @@
+// const hello = (hname)=>{
+//     console.log(`Hello, ${hname} !`);
+// }
+// hello("Hari")
+
+// Global Object
+// console.log(global);
+
+setTimeout(()=>{
+    console.log("Hello world");    //this is a timeout function it will run this string after 2 sec
+},2000)
