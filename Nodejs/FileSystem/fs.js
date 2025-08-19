ea3bcb25f929fd985b1aaa51e1771fa0006b48a6
Harinath V <140420368+Harivijay93@users.noreply.github.com>
2025-08-19 11:00:08 +0530

    Add files via upload

--- Nodejs/FileSystem/fs.js
+++ Nodejs/FileSystem/fs.js
@@ -1,8 +1,9 @@
 // File System
 
+
 const fs =require ('fs');
 
-// mkdir
+// // mkdir
 
 if ( ! fs.existsSync('./docs') )
     
@@ -16,3 +17,39 @@ if ( ! fs.existsSync('./docs') )
     })
 }
 
+
+// //// File Written
+
+fs.writeFile ('./docs/file.txt','Hello World!' ,(err) => {
+    if (err) {
+        console.log(err.message);     
+    }
+    else{
+        console.log('File Written'); 
+    }
+})
+
+//// Read file
+if(fs.existsSync('./docs/file.txt'))
+{
+    fs.readFile ('./docs/file.txt', (err, data) => {
+        if (err) 
+            console.log(err.message);
+        else
+            console.log(data.toString());
+    })
+}
+
+
+// File Delete
+
+if(fs.existsSync('./docs/file.txt'))
+{
+    fs.unlink('./docs/file.txt', (err) =>{
+        if (err) 
+            console.log(err.message);
+        else 
+            console.log('File Deleted');
+            
+    });
+}
