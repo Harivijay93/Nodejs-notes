bfb716845b5de89b79881b9a118d527f355b3332
Harinath V <140420368+Harivijay93@users.noreply.github.com>
2025-08-20 12:36:33 +0530

    Update streams.js

--- Nodejs/streams/streams.js
+++ Nodejs/streams/streams.js
@@ -3,9 +3,11 @@ const fs = require ('fs')
 
 const readhf = fs.createReadStream('./docs/hugefile.txt', {encoding: 'utf8'})
 const writehf = fs.createWriteStream('./docs/copyhugefile.txt')
-
+// first method
 readhf.on('data', (buffer) => {
     writehf.write('\nNew Buffer\n')
     writehf.write(buffer)
     
 })
+// Another method
+readhf.pipe(writehf);
