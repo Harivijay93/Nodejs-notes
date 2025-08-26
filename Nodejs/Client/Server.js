6715bc31d59d0735dbd9a6e4c1f6152c57af33e5
Harinath V <140420368+Harivijay93@users.noreply.github.com>
2025-08-26 10:14:57 +0530

    Add files via upload

--- Nodejs/Client/Server.js
+++ Nodejs/Client/Server.js
@@ -3,10 +3,14 @@ const http = require('http');
 const server= http.createServer((req, res)=>{
     console.log("Request Made");
     console.log(req.url);
-    console.log(req.method)
+    console.log(req.method);
+
+    res.setHeader('Content','text/plain');
+    res.write('Say hi to hari');
+    res.end();
+    
 });
 
 server.listen(3000, 'localhost', ()=>{
-    console.log('Server is listening');
-    
+    console.log('Server is listening');    
 })
